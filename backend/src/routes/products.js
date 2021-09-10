7409a762c28aea92ea4b0178e50a86820d0b4230
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-09-10 16:46:08 -0300

    Create Product model and consume it inside route

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { Product } = require("../models");
 
 const productsRouter = express.Router();
 
-productsRouter.get("/", (req, res) => {
-  res.sendStatus(204);
+productsRouter.get("/", async (req, res) => {
+  const products = await Product.findAll();
+
+  res.send(products);
 });
 
 module.exports.productsRouter = productsRouter;
---
+++ backend/src/seeders/20210910194243-product.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Products",
+      [
+        {
+          name: "The Minimalist Entrepreneur",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
