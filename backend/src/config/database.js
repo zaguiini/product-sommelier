b043cabd9ffeee86fca8681fb60a3b7736367fe3
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-09-12 15:36:23 -0300

    Add production database configuration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -10,4 +10,19 @@ module.exports = {
       bigNumberStrings: true,
     },
   },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
