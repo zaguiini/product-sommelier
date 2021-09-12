c39fb84b17d3fe2a42cbfba836742c6c5fe55f17
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-09-12 15:39:56 -0300

    Fix same-domain API_URI env variable

--- frontend/src/services/products.js
+++ frontend/src/services/products.js
@@ -1,13 +1,15 @@
+export const getAPIUri = () => process.env.API_URI || "";
+
 export const fetchProducts = () => {
-  return fetch(`${process.env.API_URI}/products`).then((r) => r.json());
+  return fetch(`${getAPIUri()}/products`).then((r) => r.json());
 };
 
 export const fetchProduct = ({ id }) => {
-  return fetch(`${process.env.API_URI}/products/${id}`).then((r) => r.json());
+  return fetch(`${getAPIUri()}/products/${id}`).then((r) => r.json());
 };
 
 export const addReview = ({ productId, rating, description }) => {
-  return fetch(`${process.env.API_URI}/products/${productId}/reviews`, {
+  return fetch(`${getAPIUri()}/products/${productId}/reviews`, {
     method: "POST",
     headers: {
       "Content-Type": "Application/JSON",
