e7a42ed71ea29818e14fcc47504c82059f3699cd
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-09-12 12:31:42 -0300

    Move ProductItem and products page to React

--- frontend/src/components/productItem.js
+++ frontend/src/components/productItem.js
@@ -1,17 +1,13 @@
-import ReactDOM from "react-dom";
-
-import { el, text } from "../dom";
 import { ProductRating } from "./ProductRating";
 
-export const productItem = ({ href, name, averageRating }) => {
-  const container = el("a", {
-    href,
-    class: "text-xl font-bold flex justify-between items-center mb-4",
-  });
-
-  ReactDOM.render(<ProductRating rating={averageRating} small />, container);
-
-  container.prepend(text(name));
-
-  return container;
+export const ProductItem = ({ href, name, averageRating }) => {
+  return (
+    <a
+      href={href}
+      className="text-xl font-bold flex justify-between items-center mb-4"
+    >
+      {name}
+      <ProductRating rating={averageRating} small />
+    </a>
+  );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,47 @@
-import { productItem } from "./components/productItem";
+import { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+import { ProductItem } from "./components/ProductItem";
 import { fetchProducts } from "./services/products";
 import "./styles.scss";
 
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((fetchedProducts) => {
+        setProducts(fetchedProducts);
+      })
+      .catch(() => {
+        alert("Failed to fetch products");
+      });
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <main className="flex-1 max-w-2xl mt-24 px-4 w-full">
+        <h1 className="text-4xl font-bold">Products</h1>
+        <hr className="my-6" />
+        <ul id="products-list" className="mt-3">
+          {products.map(({ id, name, averageRating }) => (
+            <ProductItem
+              key={id}
+              name={name}
+              href={`/product.html?id=${id}`}
+              averageRating={averageRating}
+            />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
 const render = async () => {
-  const products = await fetchProducts();
-
   const app = document.querySelector("#app");
-  const productsList = document.querySelector("#products-list");
 
-  products.forEach(({ id, name, averageRating }) => {
-    productsList.appendChild(
-      productItem({
-        href: `/product.html?id=${id}`,
-        name,
-        averageRating,
-      }),
-    );
-  });
-
-  app.classList.remove("hidden");
+  ReactDOM.render(<Products />, app);
 };
 
 render();
