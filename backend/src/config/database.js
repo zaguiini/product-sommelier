6279bce98a7cb6cdbf9c757b1445c8c94ee5683c
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-09-12 15:44:10 -0300

    Use environment variable for production environments

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -11,11 +11,7 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       bigNumberStrings: true,
